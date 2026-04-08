ee542a4792103f91364f17f0e996adb6b31c8a54
Yogendrareddyande <andeyogendra25@gmail.com>
2026-04-08 17:25:09 +0530

    Update streamlit_app.py
    connected changes needed bc of  move from SiS to SniS

--- streanlit_app.py
+++ streanlit_app.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 st.title(":cup_with_straw: Customize Your Smoothie")
@@ -13,7 +12,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 st.dataframe(data=my_dataframe, use_container_width=True)
 
 ingredients_list = st.multiselect('choose upto 5 ingridients:', my_dataframe,max_selections = 5)
-
+cnx = st.connection("snowflake")
+session = cnx.session()
 if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
