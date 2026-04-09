35a1c59095936323904b5f69fa1595828777c4af
Yogendrareddyande <andeyogendra25@gmail.com>
2026-04-09 10:05:35 +0530

    Fix import statement for snowflake.snowpark.context

--- streanlit_app.py
+++ streanlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.context
 st.title(":cup_with_straw: Customize Your Smoothie")
 st.write("Choose the fruits you want in your custom smoothie")
 
