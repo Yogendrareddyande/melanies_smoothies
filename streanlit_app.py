22aed8712adeea52176349d932fa25e5d15bc252
Yogendrareddyande <andeyogendra25@gmail.com>
2026-04-09 09:46:33 +0530

    Remove unused import from streanlit_app.py

--- streanlit_app.py
+++ streanlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from snowflake.snowpark.functions import col
+
 st.title(":cup_with_straw: Customize Your Smoothie")
 st.write("Choose the fruits you want in your custom smoothie")
 
